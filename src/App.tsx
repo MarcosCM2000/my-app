6513acab85f126a0bad8b86bac23617fbf72a300
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-03-02 10:56:20 -0600

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
 
-const App: React.FC = () => {
-  const todos =[{id: 't1', text: 'Finish the course'}];
-  const todoAddHandler = (text: string) => {
+import { Todo } from './models/todo';
 
+const App: React.FC = () => {
+  //  VARIAB
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text}
+    ]);
   };
   return (
     <div className="App">
