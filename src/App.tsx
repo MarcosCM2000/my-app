0803a9c77a3ace7f4efaa0863ec55bf22cc94530
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-03-02 11:17:59 -0600

    delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,23 @@ import { Todo } from './models/todo';
 const App: React.FC = () => {
   //  VARIAB
   const [todos, setTodos] = useState<Todo[]>([]);
+
   const todoAddHandler = (text: string) => {
     setTodos(prevTodos => [
       ...prevTodos,
       {id: Math.random().toString(), text}
     ]);
   };
+
+  const todoDeleteHandler = (id: string) => {
+    setTodos(prevTodos =>{
+      return prevTodos.filter(todo => todo.id !== id);
+    });
+  }
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todos}/>
+      <TodoList onDeleteTodo={todoDeleteHandler} items={todos}/>
     </div>
   );
 }
